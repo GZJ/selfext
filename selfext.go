3fbbf25e110dd196593cda946a9804db192624ec
gzj <gzj00@outlook.com>
2024-08-05 20:16:36 +0800

    refactor: add exit codes for go build and run

--- selfext.go
+++ selfext.go
@@ -33,6 +33,7 @@ func (g *Go) Build(src, dst, os, arch string) {
 	err := cmd.Run()
 	if err != nil {
 		log.Error("go build error", log.Any("error", err))
+        os.Exit(1)
 	}
 	log.Info("go build", "os", os, "arch", arch, "stdout", out.String())
 }
@@ -43,7 +44,7 @@ func (g *Go) Run(execDir string, args []string) {
 	err := cmd.Run()
 	if err != nil {
 		log.Error("go run error", log.Any("error", err))
-		return
+        os.Exit(1)
 	}
 }
 
